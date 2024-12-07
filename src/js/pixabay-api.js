d85e0c2d22343ef48aa39abba44864c393b81bfc
Anastasia_S <sivokonanastasia1@gmail.com>
2024-12-07 14:27:51 +0200

    ain

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -8,9 +8,15 @@ const BASE_URL = "https://pixabay.com/api/";
 
 export async function fetchImages(query) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`HTTP Error: ${response.status}`);
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch Images Error:", error.message);
+    throw error;
   }
-  return response.json();
 }
